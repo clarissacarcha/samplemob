2cc450d5e4f12a63acfd0241678902e7d4990f2c
Alvir <amarquez.cloudpanda@gmail.com>
2020-06-11 19:05:56 +0800

    fixing delivery dispatch

--- build/util/index.js
+++ build/util/index.js
@@ -4,4 +4,4 @@ function __export(m) {
 }
 Object.defineProperty(exports, "__esModule", { value: true });
 __export(require("./knexProfiler"));
-__export(require("./DeliveryDispatch"));
+__export(require("./deliveryDispatch"));
