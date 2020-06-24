7cfc2d103581077c21d97ee83810d5ef9dec2707
Alvir <amarquez.cloudpanda@gmail.com>
2020-06-24 19:50:31 +0800

    added ts-nochecks

--- controllers/RequirementType.ts
+++ controllers/RequirementType.ts
@@ -1,14 +1,14 @@
-import { RequirementTypeModel } from '../rest-models/RequirementTypeModel';
+//@ts-nocheck
+import { RequirementTypeModel } from "../rest-models/RequirementTypeModel";
 
-import { AuthUtility } from '../util/AuthUtility';
+import { AuthUtility } from "../util/AuthUtility";
 
-import {check, validationResult} from 'express-validator';
+import { check, validationResult } from "express-validator";
 
-import { ServerResponse } from '../interfaces/ServerResponse'; 
+import { ServerResponse } from "../interfaces/ServerResponse";
 
-export class RequirementType{
-
-  static create = async (req:any,res:any,next:any) =>{
+export class RequirementType {
+  static create = async (req: any, res: any, next: any) => {
     let status = 200;
 
     //validate input
@@ -16,30 +16,22 @@ export class RequirementType{
     if (!errors.isEmpty()) {
       status = 422;
       return res.status(200).json(
-         new ServerResponse(
-           status,
-           {
-             errors: errors.array()
-           }
-         ).sendResponse()
+        new ServerResponse(status, {
+          errors: errors.array(),
+        }).sendResponse()
       );
     }
 
     let result = await RequirementTypeModel.create(req.body);
 
     res.status(200).json(
-        new ServerResponse(
-           status,
-           {
-             result
-           }
-         ).sendResponse()
+      new ServerResponse(status, {
+        result,
+      }).sendResponse()
     );
+  };
 
-  }
-
-  static read = async (req:any,res:any,next:any) =>{
-
+  static read = async (req: any, res: any, next: any) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() });
@@ -48,13 +40,11 @@ export class RequirementType{
     let result = await RequirementTypeModel.read(req.params.id);
 
     res.status(200).json({
-        result
+      result,
     });
+  };
 
-  }
-
-  static update = async (req:any,res:any,next:any) =>{
-
+  static update = async (req: any, res: any, next: any) => {
     let status = 200;
 
     //validate input
@@ -62,29 +52,22 @@ export class RequirementType{
     if (!errors.isEmpty()) {
       status = 422;
       return res.status(200).json(
-         new ServerResponse(
-           status,
-           {
-             errors: errors.array()
-           }
-         ).sendResponse()
+        new ServerResponse(status, {
+          errors: errors.array(),
+        }).sendResponse()
       );
     }
 
     let result = await RequirementTypeModel.update(req.body);
 
     res.status(200).json(
-        new ServerResponse(
-           status,
-           {
-             result
-           }
-         ).sendResponse()
+      new ServerResponse(status, {
+        result,
+      }).sendResponse()
     );
-  }
+  };
 
-  static delete = async (req:any,res:any,next:any) =>{
-
+  static delete = async (req: any, res: any, next: any) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() });
@@ -93,19 +76,15 @@ export class RequirementType{
     let result = await RequirementTypeModel.delete(req.params.id);
 
     res.status(200).json({
-      result
+      result,
     });
+  };
 
-  }
-
-
-  static list = async (req:any,res:any,next:any) =>{
-
+  static list = async (req: any, res: any, next: any) => {
     //varify access
     let ress = await AuthUtility.verifyAccess(req.body.token);
 
-    if(ress)
-    {
+    if (ress) {
       //validate input
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
@@ -115,19 +94,16 @@ export class RequirementType{
       let result = await RequirementTypeModel.list(req.body);
 
       res.status(200).json({
-        result
+        result,
       });
-    }
-    else{
+    } else {
       let result = {
         authError: 1,
-        message: "You are not logged in"
-      }
+        message: "You are not logged in",
+      };
       res.status(200).json({
-        result
+        result,
       });
     }
-
-  }
-
+  };
 }
