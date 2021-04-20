b88221fb217056fd44152629418b941b7b86d7a0
Alvir Marquez <amarquez.cloudpanda@gmail.com>
2021-04-20 16:31:04 +0800

    Adjusting margins

--- src/revamp/buttons/BlackButton.js
+++ src/revamp/buttons/BlackButton.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {View, Text, StyleSheet, TouchableHighlight} from 'react-native';
-import {COLOR, DARK, COLOR_UNDERLAY, FONT_REGULAR} from '../../res/constants';
+import {COLOR, DARK, COLOR_UNDERLAY, FONT_REGULAR, FONT_MEDIUM, FONT_BOLD, FONT_SEMIBOLD} from '../../res/constants';
 
-export const BlackButton = ({label, onPress, style}) => {
+export const BlackButton = ({label, onPress, style, touchableStyle = {}}) => {
   return (
-    <TouchableHighlight onPress={onPress} style={styles.blackButton} underlayColor={COLOR}>
+    <TouchableHighlight onPress={onPress} style={[styles.blackButton, touchableStyle]} underlayColor={COLOR}>
       <View style={[styles.blackButtonBox, style]}>
         <Text style={styles.label}>{label}</Text>
       </View>
@@ -25,8 +25,8 @@ const styles = StyleSheet.create({
   },
   label: {
     color: COLOR,
-    fontSize: 14 ,
+    fontSize: 14,
     paddingHorizontal: 10,
-    fontFamily: FONT_REGULAR
+    fontFamily: FONT_MEDIUM,
   },
 });
