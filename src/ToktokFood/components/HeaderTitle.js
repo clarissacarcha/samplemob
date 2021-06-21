5ee175888e5151050d9910dfa1b2b0cda601aa7c
Gerald Cells <diazgerald13@gmail.com>
2021-06-21 19:35:09 +0800

    add icons for search box

--- src/ToktokFood/components/HeaderTitle.js
+++ src/ToktokFood/components/HeaderTitle.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import {View, StyleSheet, Text, TouchableOpacity} from 'react-native';
+import {useSelector} from 'react-redux';
 import FIcon5 from 'react-native-vector-icons/FontAwesome5';
 import ContentLoader from 'react-native-easy-content-loader';
-import {useSelector} from 'react-redux';
+import {Image, View, StyleSheet, Text, TouchableOpacity} from 'react-native';
+
+import {FONT, FONT_SIZE, COLOR} from '../../res/variables';
+import {markerIcon} from '../assets/images';
 
 const HeaderTitle = () => {
   const {location} = useSelector((state) => state.toktokFood);
 
   const renderText = () => (
-    <>
+    <View style={styles.addressContainer}>
       <Text style={styles.headerLabel}>TokTok PH</Text>
-      <Text numberOfLines={1}>{location.formattedAddress}</Text>
-    </>
+      <View style={styles.textAddressContainer}>
+        <Image style={styles.addressMarkerIcon} source={markerIcon} />
+        <Text style={styles.textAddress} numberOfLines={2}>
+          {location.formattedAddress}
+        </Text>
+      </View>
+    </View>
   );
 
   const renderLoader = () => (
@@ -39,18 +47,39 @@ export default HeaderTitle;
 
 const styles = StyleSheet.create({
   header: {
+    marginTop: 45,
     flexDirection: 'row',
-    paddingVertical: 40,
   },
   headerBack: {
     paddingHorizontal: 20,
     justifyContent: 'center',
   },
   headerLabel: {
-    fontWeight: '500',
-    fontSize: 15,
+    marginLeft: 2,
+    color: COLOR.BLACK,
+    fontSize: FONT_SIZE.XL,
+    fontFamily: FONT.BOLD,
   },
   headerTextContainer: {
     flex: 1,
+  },
+  addressContainer: {
+    paddingRight: 10,
+  },
+  textAddressContainer: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  textAddress: {
+    color: COLOR.BLACK,
+    fontSize: FONT_SIZE.M,
+    fontFamily: FONT.REGULAR,
+  },
+  addressMarkerIcon: {
+    width: 18,
+    height: 18,
+    marginRight: 4,
   },
 });
