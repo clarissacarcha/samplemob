05991c1c9a5e1f823c11cadafd355ddfa9cebd57
BP Altezo <bpaltezo@cloudpanda.ph>
2021-06-23 10:20:10 +0800

    Toktokmall homepage Carousel Component

---
+++ src/ToktokMall/Components/Widgets/Carousel.js
@@ -0,0 +1,61 @@
+import React, {useState, useEffect, useRef} from 'react';
+import {View, Text, ImageBackground, Image} from 'react-native';
+import Carousel, { Pagination } from 'react-native-snap-carousel';
+import CustomIcon from '../Icons';
+
+export const AdsCarousel = (props) => {
+
+  const [activeSlide, setActiveSlide] = useState(0)
+  const [entries, setEntries] = useState([1, 2, 3])
+
+  const renderItem = ({item, index}) => {
+    return (
+      <View>
+        <Image source={require("../../../assets/images/sampleAds/ad1.png")} style={{width: '100%', height: 170, resizeMode: 'stretch'}} />
+      </View>
+    )
+  }
+
+  return (
+    <View>
+      <Carousel
+        data={entries}
+        renderItem={renderItem}
+        onSnapToItem={(index) => setActiveSlide(index) }
+        sliderWidth={330}
+        itemWidth={330}
+      />
+      <Pagination
+        dotsLength={entries.length}
+        activeDotIndex={activeSlide}
+        renderDots={(activeIndex) => {
+          return (
+            <View style={{flex: 1, flexDirection: 'row', backgroundColor: 'transparent', alignItems: 'center',justifyContent: 'center', bottom: "-200%"}}>
+              {entries.map((d, i) => <View style={{paddingHorizontal: 8}}>
+                <CustomIcon.MCIcon name={i == activeIndex ? "circle" : "circle-outline"} color="#fff" size={10} />
+              </View>)}              
+            </View>
+          )
+        }}
+        containerStyle={{ backgroundColor: 'transparent', position: 'absolute', bottom: 0, alignSelf: 'center', alignContent: 'flex-end' }}
+        // dotStyle={{
+        //   width: 10,
+        //   height: 10,
+        //   borderRadius: 5,
+        //   marginHorizontal: 0,
+        //   backgroundColor: '#fff'
+        // }}
+        // dotContainerStyle={{
+        //   padding: 0
+        // }}
+        // inactiveDotStyle={{
+        //   borderColor: '#fff'
+        //   // Define styles for inactive dots here
+        // }}
+        // inactiveDotOpacity={0.4}
+        // inactiveDotScale={0.6}
+      />
+    </View>
+  )
+
+}
--- src/ToktokMall/screens/LandingScreens/ToktokMallHome/ToktokMallLanding/index.js
+++ src/ToktokMall/screens/LandingScreens/ToktokMallHome/ToktokMallLanding/index.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import {View, Text, ImageBackground, Image} from 'react-native';
 import FIcon5 from 'react-native-vector-icons/FontAwesome';
 import AIcon from 'react-native-vector-icons/MaterialIcons';
 import { COLOR, FONT } from '../../../../../res/variables';
-import {LandingHeader} from '../../../../Components';
+import {LandingHeader, AdsCarousel} from '../../../../Components';
+import Carousel from 'react-native-snap-carousel';
+import { ScrollView } from 'react-native-gesture-handler';
 
 export const ToktokMallLandingScreen = () => {
+
+  const adsRef = useRef();
+
   return (
     <View style={{flex: 1, backgroundColor: '#fff'}}>
       
       <LandingHeader />
 
-      <View style={{flex: 1}}>
-        <Text>Content Here</Text>
-      </View>
+      <ScrollView vertical={true} showsVerticalScrollIndicator={false} style={{flex: 1, paddingHorizontal: 15, paddingVertical: 15}}>
+
+        <AdsCarousel data={[1,2,3]} />
+
+      </ScrollView>
 
     </View>
   );
