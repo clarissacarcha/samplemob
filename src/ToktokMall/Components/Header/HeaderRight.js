991a53855ae2093a0c5147922343ea2d45f9e68d
BP Altezo <bpaltezo@cloudpanda.ph>
2021-06-23 21:40:47 +0800

    Categories list screen

--- src/ToktokMall/Components/Header/HeaderRight.js
+++ src/ToktokMall/Components/Header/HeaderRight.js
@@ -2,11 +2,10 @@ import React from 'react';
 import {StyleSheet, View, TouchableOpacity,Text } from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import {throttle} from 'lodash';
-import FIcon5 from 'react-native-vector-icons/FontAwesome5';
+import CustomIcon from '../Icons';
 import { COLOR, FONT } from '../../../res/variables'; 
 
-
-export const HeaderRight = ({ color = "black" , label, hidden, icon, onPress}) => {
+export const HeaderRight = ({ color = "black" , label, hidden, icon, iconSize, onPress}) => {
   const navigation = useNavigation();
 
   const onButtonPress = throttle(
@@ -23,7 +22,7 @@ export const HeaderRight = ({ color = "black" , label, hidden, icon, onPress}) =
 
   return (
     <TouchableOpacity onPress={!hidden && onButtonPress} style={{paddingHorizontal: 15,flex: 0,justifyContent:"flex-start",alignItems:'center',flexDirection:"row"}}>
-        {!hidden && icon ? <FIcon5 name={icon} color={COLOR.ORANGE} size={13}/> : null}
+        {!hidden && icon ? <CustomIcon.FoIcon name={icon} color={COLOR.ORANGE} size={iconSize}/> : null}
         <Text style={{fontFamily: FONT.REGULAR,fontSize: 14,marginLeft: 5, color: COLOR.ORANGE}}>{label}</Text>
     </TouchableOpacity>
   );
