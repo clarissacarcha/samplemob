768e80bb808d74da3ccd1553962914c88cd58623
BP Altezo <bpaltezo@cloudpanda.ph>
2021-07-24 16:46:28 +0800

    toktokmall MD

---
+++ TokMall.md
@@ -0,0 +1,6 @@
+# Checkout
+
+# Tables to update
+1. app_order_logs
+2. app_order_details
+3. app_order_branch_details
--- src/ToktokMall/screens/RootScreens/ToktokMallRateProduct/index.js
+++ src/ToktokMall/screens/RootScreens/ToktokMallRateProduct/index.js
@@ -1,121 +1,12 @@
 import React from 'react'
-import { Text, View, Animated, StyleSheet } from 'react-native'
-import StickyParallaxHeader from 'react-native-sticky-parallax-header'
-import { LandingSubHeader, LandingHeader } from '../../../Components'
+import {View, Text} from 'react-native'
 
-export const ToktokMallRateProduct = ({navigation}) => {
-
-  const [scroll, setScroll] = React.useState(new Animated.Value(0))
-  const [_value, set_Value] = React.useState(0)
-
-  React.useEffect(() => {
-    scroll.addListener(({value}) => set_Value(value))
-  }, [])
-
-  const renderContent = () => {
-    return (
-      <>
-        <View style={styles.content}>
-          <Text>{label}</Text>
-        </View>
-      </>
-    )
-  }
-  
-  const renderForeground = () => {
-    
-    const titleOpacity = scroll.interpolate({
-      inputRange: [0, 106, 154],
-      outputRange: [1, 1, 0],
-      extrapolate: 'clamp'
-    })
-  
-    return (
-      <View style={{backgroundColor: 'transparent'}}>
-        <Animated.View style={{ opacity: titleOpacity }}>
-          <LandingHeader />
-        </Animated.View>
-      </View>
-    )
-  }
-  
-  const renderHeader = () => {
-  
-    const opacity = scroll.interpolate({
-      inputRange: [0, 120, 170],
-      outputRange: [0, 0, 1],
-      extrapolate: 'clamp'
-    })
-  
-    return (
-      <View style={{backgroundColor: 'transparent'}}>
-        <Animated.View style={{ opacity }}>
-          <LandingSubHeader />
-        </Animated.View>
-      </View>
-    )
-  }
-
+export const ToktokMallRateProduct = () => {
   return (
-    <>
-      <StickyParallaxHeader
-        foreground={renderForeground()}
-        header={renderHeader()}
-        parallaxHeight={200}
-        headerHeight={0}
-        headerSize={() => {}}
-        onEndReached={() => {}}
-        scrollEvent={Animated.event([{ nativeEvent: { contentOffset: { y: scroll } } }])}        
-      >
-      </StickyParallaxHeader>
-    </>
+		<>
+			<View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+				<Text>Rate Product</Text>
+			</View>
+		</>
   )
 }
-
-const styles = StyleSheet.create({
-  content: {
-    height: 1000,
-    marginTop: 50
-  },
-  foreground: {
-    flex: 0.2,
-    justifyContent: 'flex-end',
-    backgroundColor: 'red'
-  },
-  message: {
-    color: 'white',
-    fontSize: 40,
-    paddingTop: 24,
-    paddingBottom: 7
-  },
-  headerWrapper: {
-    backgroundColor: 'green',
-    width: '100%',
-    paddingHorizontal: 24,
-    paddingBottom: 25,
-    flexDirection: 'row',
-    alignItems: 'center'
-  },
-  headerTitle: {
-    fontSize: 16,
-    color: 'white',
-    margin: 12
-  },
-  tabsWrapper: {
-    paddingVertical: 12
-  },
-  tabTextContainerStyle: {
-    backgroundColor: 'transparent',
-    borderRadius: 18
-  },
-  tabTextContainerActiveStyle: {
-    backgroundColor: 'lightgreen'
-  },
-  tabText: {
-    fontSize: 16,
-    lineHeight: 20,
-    paddingHorizontal: 12,
-    paddingVertical: 8,
-    color: 'white'
-  }
-})
