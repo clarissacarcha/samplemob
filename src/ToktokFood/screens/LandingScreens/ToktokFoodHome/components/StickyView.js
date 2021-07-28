f2d6b880abd2d5fa75d76cbad27575fbabcb826d
Raven <raveenlucas03@gmail.com>
2021-07-28 13:32:26 +0800

    feat(home): home category fix

--- src/ToktokFood/screens/LandingScreens/ToktokFoodHome/components/StickyView.js
+++ src/ToktokFood/screens/LandingScreens/ToktokFoodHome/components/StickyView.js
@@ -34,8 +34,8 @@ const tabs = [
 const StickyView = () => {
   const [offset, setOffset] = useState(0);
   const [activeTab, setActiveTab] = useState(tabs[0]);
-  const headerMaxHeight = Platform.OS === 'ios' ? moderateScale(160) : scale(190);
-  const headerMinHeight = Platform.OS === 'ios' ? verticalScale(42) : moderateScale(65);
+  const headerMaxHeight = Platform.OS === 'ios' ? moderateScale(130) : scale(190);
+  const headerMinHeight = Platform.OS === 'ios' ? verticalScale(43) : moderateScale(65);
   const {categories} = useSelector((state) => state).toktokFood;
   const {shops} = useSelector((state) => state).toktokFood;
 
