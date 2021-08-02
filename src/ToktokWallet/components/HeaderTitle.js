905d105f6460cbeb588b7b2785dfb8e8aecdd5ea
Clarissa Carcha <ccarcha@cloudpanda.ph>
2021-08-02 17:37:39 +0800

    Fix error catch when no internet connection in cash in payment option and terms and condition screen

--- src/ToktokWallet/components/HeaderTitle.js
+++ src/ToktokWallet/components/HeaderTitle.js
@@ -4,23 +4,21 @@ import CONSTANTS from 'common/res/constants'
 import { HeaderBack } from 'src/revamp'
 const { COLOR , FONT_FAMILY: FONT , FONT_SIZE } = CONSTANTS
 
-export const HeaderTitle = ({isLogo, label ,labelColor = "black" , backButtonColor = "black" , headerBackLabel = ""})=> {
+export const HeaderTitle = ({isLogo, label ,labelColor = "black" , backButtonColor = "black" , headerBackLabel = "", headerStyle = {}})=> {
 
     return (
-        <View style={styles.header}>
-                <View style={{flex: 1,flexDirection:"row"}}>
-                    <HeaderBack color={backButtonColor} label={headerBackLabel}/>
-                </View>
-                <View style={{width: 150,justifyContent:"center",alignItems:"center"}}>
-                {
-                    isLogo
-                    ? <Image resizeMode="contain" style={{height: 23,width: 130}} source={require('toktokwallet/assets/images/toktokwallet.png')} />
-                    : <Text style={{fontSize: FONT_SIZE.L,fontFamily: FONT.BOLD,color: labelColor}}>{label}</Text>
-                }
-                </View>
-                <View style={{flex: 1}}>
-
-                </View>
+        <View style={[styles.header, headerStyle ]}>
+            <View style={{flex: 1,flexDirection:"row"}}>
+                <HeaderBack color={backButtonColor} label={headerBackLabel}/>
+            </View>
+            <View style={{width: 150,justifyContent:"center",alignItems:"center"}}>
+            {
+                isLogo
+                ? <Image resizeMode="contain" style={{height: 23,width: 130}} source={require('toktokwallet/assets/images/toktokwallet.png')} />
+                : <Text style={{fontSize: FONT_SIZE.L,fontFamily: FONT.BOLD,color: labelColor}}>{label}</Text>
+            }
+            </View>
+            <View style={{flex: 1}} />
         </View>
     )
 }
