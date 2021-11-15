0f98280acbe41a08cf129cf12b578532fc84dc97
Clarissa Carcha <ccarcha@cloudpanda.ph>
2021-11-15 10:44:34 +0800

    feature: biller list design

--- src/ToktokBills/screens/RootScreens/ToktokBiller/index.js
+++ src/ToktokBills/screens/RootScreens/ToktokBiller/index.js
@@ -1,43 +1,90 @@
-import React , {useEffect} from 'react'
+import React , {useEffect, useState} from 'react'
 import { View , Text , StyleSheet , Platform , FlatList, Dimensions , Image , TouchableHighlight} from 'react-native'
-import { HeaderBack , HeaderTitle , Separator } from 'toktokbills/components'
+import { HeaderBack , HeaderTitle , Separator, SearchInput } from 'toktokbills/components'
 import { moderateScale } from 'toktokbills/helper'
 import { usePrompt , useThrottle } from 'src/hooks'
+import { Biller } from "./Components"
+
+const sampleData = [
+  {
+    name: "Philippine Long Distance and Telephone",
+    logo: "toktokwallet/assets/image/tokwa.png"
+  },
+  {
+    name: "Cable/Internet",
+    logo: "toktokwallet/assets/image/tokwa.png"
+  },
+  {
+    name: "Cable/Internet",
+    logo: "toktokwallet/assets/image/tokwa.png"
+  },
+  {
+    name: "Cable/Internet sfsd sdfd",
+    logo: "toktokwallet/assets/image/tokwa.png"
+  },
+  {
+    name: "Cable/Internet",
+    logo: "toktokwallet/assets/image/tokwa.png"
+  },
+  {
+    name: "Cable/Internet",
+    logo: "toktokwallet/assets/image/tokwa.png"
+  },
+]
 
 export const ToktokBiller = ({navigation, route})=> {
-    const { biller } = route.params
-    const prompt = usePrompt()
+  const { biller } = route.params
+  const prompt = usePrompt()
 
-    navigation.setOptions({
-        headerLeft: () => <HeaderBack />,
-        headerTitle: () => <HeaderTitle label={biller.name} isRightIcon/>,
-        headerStyle: { height: Platform.OS == 'ios' ? moderateScale(60) : moderateScale(80) }
-    });
-    
+  navigation.setOptions({
+    headerLeft: () => <HeaderBack />,
+    headerTitle: () => <HeaderTitle label={biller.name} isRightIcon/>,
+    headerStyle: { height: Platform.OS == 'ios' ? moderateScale(60) : moderateScale(80) }
+  });
 
-    return (
-        <>
-        <Separator/>
-        <View style={styles.container}>
-            <TouchableHighlight hitSlop={{top: 100 , bottom: 100 ,right: 100, left: 100}} onPress={()=> prompt({
-                type: "success",
-                title: "Payment Successful",
-                message: `Your payment to PLDT amounting to ₱ 2100.00 has been successfully processed with ref no. 0987654321 on September 15, 2021, 5:00 pm.`,
-                onPress: ()=> navigation.navigate("ToktokBillsEnterOTP")
-            })}>
-                <View>
-                    <Text>gg</Text>
-                </View>
-            </TouchableHighlight>
-        </View>
-        </>
-    )
+  const [search, setSearch] = useState("");
+  const [filteredData, setFilteredData] = useState("");
+
+  useEffect(() => {
+    if(search != ""){
+      const filteredContacts = sampleData.filter((item) => {
+        return item.name.toLowerCase().includes(search.toLowerCase())
+      });
+      setFilteredData(filteredContacts);
+    } else {
+      setFilteredData([])
+    }
+  }, [search])
+
+
+  return (
+    <>
+    <Separator/>
+    <View style={styles.container}>
+      <View style={styles.searchContainer}>
+        <SearchInput search={search} setSearch={setSearch} />
+      </View>
+      <FlatList
+        data={filteredData.length > 0 ? filteredData : sampleData}
+        renderItem={({item,index})=><Biller item={item} index={index}/>}
+        contentContainerStyle={styles.listContainer}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </View>
+    </>
+  )
 }
 
 const styles = StyleSheet.create({
-    container : {
-        flex: 1,
-        backgroundColor:"white",
-        padding: moderateScale(16)
-    }
+  container : {
+    flex: 1,
+    backgroundColor:"white",
+  },
+  searchContainer: {
+    padding: moderateScale(16),
+  },
+  listContainer: {
+    paddingHorizontal: moderateScale(16),
+    paddingBottom: moderateScale(16),
+  }
 })
