b25b87ee0ca2416dcbe9564799900fbcdb23555f
Earl <earl@Earls-MacBook-Pro.local>
2022-01-17 13:24:09 +0800

    fix: reseller's discout UI

--- src/ToktokMall/Components/Widgets/RefComDiscountRate.js
+++ src/ToktokMall/Components/Widgets/RefComDiscountRate.js
@@ -1,12 +1,39 @@
-import React from 'react'
-import {StyleSheet, View, Text, ImageBackground, Image, TouchableOpacity, FlatList, ActivityIndicator} from 'react-native';
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
 
 export const RefComDiscountRate = ({value, w = '50%'}) => {
-	return (
-		<>
-			<View style={{flex: 0, width: w}}>
-        <Text style={{fontSize: 9, fontWeight: "700", color: "#fff", backgroundColor: '#FF0F00', paddingHorizontal: 4, paddingVertical: 2, borderRadius: 3, textAlign: 'center'}}>{value}</Text>
+  return (
+    <>
+      <View style={{width: w, marginTop: 5}}>
+        <Text
+          style={{
+            fontSize: 9,
+            fontWeight: '700',
+            backgroundColor: '#FDBA1C',
+            borderRadius: 3,
+            textAlign: 'center',
+            marginBottom: -16,
+            marginLeft: -3,
+            marginRight: 3,
+          }}
+        />
+        <Text
+          style={{
+            fontSize: 9,
+            fontWeight: '700',
+            color: '#fff',
+            backgroundColor: '#FF0F00',
+            paddingHorizontal: 4,
+            paddingVertical: 2,
+            borderRadius: 3,
+            textAlign: 'center',
+          }}>
+          {value}
+        </Text>
       </View>
-		</>
-	)
-}
+    </>
+  );
+};
