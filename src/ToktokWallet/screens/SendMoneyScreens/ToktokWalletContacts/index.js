27b5561fbbff8cdd911ce8e6f24da58fa040ace1
Alvin Raquem <araquem.cloudpanda@gmail.com>
2022-01-18 19:36:16 +0800

    add search icon in all contacts

--- src/ToktokWallet/screens/SendMoneyScreens/ToktokWalletContacts/index.js
+++ src/ToktokWallet/screens/SendMoneyScreens/ToktokWalletContacts/index.js
@@ -6,7 +6,7 @@ import {useSelector} from 'react-redux'
 import Contacts from 'react-native-contacts'
 import {sortBy} from 'lodash'
 import {Separator} from 'toktokwallet/components'
-import { HeaderBack , HeaderTitle } from 'src/revamp'
+import { HeaderBack , HeaderTitle , ICON_SET, VectorIcon } from 'src/revamp'
 import CONSTANTS from 'common/res/constants'
 
 const { COLOR , FONT_FAMILY: FONT , FONT_SIZE , SIZE , MARGIN } = CONSTANTS
@@ -197,7 +197,9 @@ export const ToktokWalletContacts = ({navigation,route})=> {
       <View style={styles.container}>
         <View style={styles.searchField}>
             <View style={[styles.input,{flexDirection: "row"}]}>
-                {/* <Image style={{height: 25,width: 40,alignSelf: "center"}} resizeMode="center" source={require('../../../../../../assets/icons/ph.png')}/> */}
+               <View style={{justifyContent:"center"}}> 
+                <VectorIcon size={28} iconSet={ICON_SET.Feather} name="search"/>
+               </View>
                 <TextInput 
                     style={{color: COLOR.DARK,fontSize: FONT_SIZE.M,fontFamily: FONT.REGULAR,padding: 0,marginLeft: 5,alignSelf: "center",flex: 1}}
                     placeholder="Enter a Name or mobile number"
