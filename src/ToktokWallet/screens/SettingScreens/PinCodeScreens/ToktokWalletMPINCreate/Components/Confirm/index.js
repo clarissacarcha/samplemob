66de42188ee831d60fc46d707132be4c4318699c
Alvin Raquem <araquem.cloudpanda@gmail.com>
2022-01-24 16:25:25 +0800

    add informative message in MPIN setup

--- src/ToktokWallet/screens/SettingScreens/PinCodeScreens/ToktokWalletMPINCreate/Components/Confirm/index.js
+++ src/ToktokWallet/screens/SettingScreens/PinCodeScreens/ToktokWalletMPINCreate/Components/Confirm/index.js
@@ -40,8 +40,8 @@ export const Confirm = ({pinCode,setPageIndex,walletinfo,patchPincodeToktokWalle
     return (
        <View style={styles.container}>
             <ScrollView style={styles.content}>
-            {/* <Text style={{marginVertical: 20, marginHorizontal: 10 ,textAlign:"center",fontFamily: FONT.REGULAR, fontSize: FONT_SIZE.S}}>Ka-toktok, do not forget your MPIN, keep it to yourself and do not
-share this with anyone</Text> */}
+            <Text style={{marginVertical: 20, marginHorizontal: 10 ,textAlign:"center",fontFamily: FONT.REGULAR, fontSize: FONT_SIZE.S}}>Ka-toktok, do not forget your MPIN, keep it to yourself and do not
+share this with anyone</Text>
                     <Text style={{fontSize: FONT_SIZE.M,fontFamily: FONT.BOLD,marginTop: 20,alignSelf:"center"}}>Confirm {tokwaAccount.mpinCode ? "New ": ""}MPIN</Text>
                     <View style={{position: 'relative',marginTop: 50,}}>
                         <NumberBoxes pinCode={confirmpinCode} onNumPress={onNumPress} showPin={showPin} numberOfBox={4}/>
