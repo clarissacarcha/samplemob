81e4a3cfcbea3c57e9c0d09550a74b72ba9902c5
Raven <raveenlucas03@gmail.com>
2022-01-27 14:14:57 +0800

    fix(cashin): cash in navigation fix

--- src/ToktokFood/screens/LandingScreens/ToktokFoodMe/index.js
+++ src/ToktokFood/screens/LandingScreens/ToktokFoodMe/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, {useState, useEffect, useContext} from 'react';
 import {Platform, StyleSheet, View, Image, Text, FlatList, TouchableOpacity} from 'react-native';
 import FIcon5 from 'react-native-vector-icons/FontAwesome5';
@@ -13,13 +14,13 @@ import {useUserLocation} from 'toktokfood/hooks';
 import {useSelector} from 'react-redux';
 import {moderateScale, getStatusbarHeight} from 'toktokfood/helper/scale';
 import {arrow_right, help_centre_ic} from 'toktokfood/assets/images';
-import { GET_MY_ACCOUNT } from 'toktokwallet/graphql';
-import { TOKTOK_WALLET_GRAPHQL_CLIENT } from 'src/graphql';
-import { useLazyQuery } from '@apollo/react-hooks';
+import {GET_MY_ACCOUNT} from 'toktokwallet/graphql';
+import {TOKTOK_WALLET_GRAPHQL_CLIENT} from 'src/graphql';
+import {useLazyQuery} from '@apollo/react-hooks';
 import {useIsFocused, useNavigation} from '@react-navigation/native';
 // Fonts & Colors
 import {COLOR, FONT, FONT_SIZE} from 'res/variables';
-import { HelpCentre, Me, VerifyContext, VerifyContextProvider } from './components';
+import {HelpCentre, Me, VerifyContext, VerifyContextProvider} from './components';
 
 const CUSTOM_HEADER = {
   container: Platform.OS === 'android' ? moderateScale(70) + getStatusbarHeight : moderateScale(70),
@@ -27,57 +28,59 @@ const CUSTOM_HEADER = {
 };
 
 const MainComponent = () => {
-
-  const {location} = useSelector((state) => state.toktokFood);
-  const {user} = useSelector((state) => state.session);
-  const { showHelpCentreList, setShowHelpCentreList, walletBalance, setWalletBalance } = useContext(VerifyContext);
+  const {location} = useSelector(state => state.toktokFood);
+  const {user} = useSelector(state => state.session);
+  const {showHelpCentreList, setShowHelpCentreList, walletBalance, setWalletBalance} = useContext(VerifyContext);
   const isFocus = useIsFocused();
   const navigation = useNavigation();
-  const [ getMyAccount, {loading, error} ] = useLazyQuery(GET_MY_ACCOUNT , {
+  const [getMyAccount, {loading, error}] = useLazyQuery(GET_MY_ACCOUNT, {
     fetchPolicy: 'network-only',
     client: TOKTOK_WALLET_GRAPHQL_CLIENT,
-    onCompleted: ({ getMyAccount })=> {
+    onCompleted: ({getMyAccount}) => {
       setWalletBalance(getMyAccount.wallet.balance);
     },
-    onError: (error) => {
+    onError: error => {
       // console.log(error)
-    }
+    },
   });
 
   useEffect(() => {
-    if(user && isFocus){
-      if(user.toktokWalletAccountId){
-        getMyAccount()
+    if (user && isFocus) {
+      if (user.toktokWalletAccountId) {
+        getMyAccount();
       } else {
         setWalletBalance(null);
       }
-    } 
-  }, [user, isFocus])
+    }
+  }, [user, isFocus]);
 
   const onBack = () => {
     // setShowHelpCentreList(false)
     navigation.goBack();
-  }
+  };
 
   return (
     <View style={styles.container}>
       <HeaderImageBackground searchBox={false}>
         <View style={styles.header}>
           {/* { showHelpCentreList && ( */}
-            <TouchableOpacity onPress={onBack}>
-              <FIcon5 name="chevron-left" size={15} />
-            </TouchableOpacity>
+          <TouchableOpacity onPress={onBack}>
+            <FIcon5 name="chevron-left" size={15} />
+          </TouchableOpacity>
           {/* )} */}
-          <Image source={{ uri: user.person.avatar }} style={styles.avatar} />
-          <Text style={styles.name}>
-            {`${user.person.firstName} ${user.person.lastName}`}
-          </Text>
+          <Image source={{uri: user.person.avatar}} style={styles.avatar} />
+          <Text style={styles.name}>{`${user.person.firstName} ${user.person.lastName}`}</Text>
         </View>
       </HeaderImageBackground>
-      { loading || error ? (
-          <LoadingIndicator isFlex isLoading={true} />
-        ): (
-          <HelpCentre getMyAccount={() => { getMyAccount() }} />
+      {loading || error ? (
+        <LoadingIndicator isFlex isLoading={true} />
+      ) : (
+        <HelpCentre
+          loading={loading}
+          getMyAccount={() => {
+            getMyAccount();
+          }}
+        />
       )}
     </View>
   );
@@ -109,11 +112,11 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     paddingTop: getStatusbarHeight,
-    paddingHorizontal: 16
+    paddingHorizontal: 16,
   },
   name: {
     fontSize: FONT_SIZE.L,
     fontFamily: FONT.BOLD,
-    paddingHorizontal: 15
-  }
+    paddingHorizontal: 15,
+  },
 });
