06feefb78dfba4e6ce61e42353f72a9f3b017576
Clarissa Carcha <ccarcha@cloudpanda.ph>
2022-03-01 12:43:55 +0800

    change transaction date and time to transaction date

--- src/ToktokLoad/screens/GlobalScreens/ToktokLoadReceipt/components/ReceiptDetails/index.js
+++ src/ToktokLoad/screens/GlobalScreens/ToktokLoadReceipt/components/ReceiptDetails/index.js
@@ -27,7 +27,7 @@ export const ReceiptDetails = ({ route }) => {
           <Text style={styles.description}>{referenceNumber}</Text>
         </View>
         <View style={[ styles.bodyContainer, styles.marginBottom15 ]}>
-          <Text style={styles.title}>Transaction Date and Time </Text>
+          <Text style={styles.title}>Transaction Date </Text>
           <Text style={styles.description}>{transactionDateTime}</Text>
         </View>
         <View style={[ styles.bodyContainer, styles.marginBottom15 ]}>
