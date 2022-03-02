bfe6c848850aa52b177ea2eab8bf88f1e8af2d8b
Zion Camba <zcamba@cloudpanda.ph>
2022-03-02 19:42:37 +0800

    added booking details screen

---
+++ src/ToktokGo/screens/BookingDetails/SelectedBookingDetails.js
@@ -0,0 +1,106 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { ScrollView, StyleSheet, Text, TextInput, View, Dimensions, Image } from 'react-native'
+import { 
+    HeaderBack, 
+    HeaderTitle } from '../../../components/';
+import CONSTANTS from '../../../common/res/constants';
+import { TouchableOpacity } from '@gorhom/bottom-sheet';
+import EIcons from 'react-native-vector-icons/EvilIcons';
+import AntIcons from 'react-native-vector-icons/AntDesign';
+import MCIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { PATCH_USER_CHANGE_PASSWORD } from '../../../../graphql';
+import Toast from 'react-native-simple-toast';
+import { useMutation } from '@apollo/react-hooks';
+import MIcon from 'react-native-vector-icons/MaterialIcons';
+import {SIZES, FONT_MEDIUM, COLORS, BUTTON_HEIGHT, NUMBERS} from '../../../res/constants'
+import { connect } from 'react-redux';
+import AntIcon from "react-native-vector-icons/AntDesign";
+import { onError } from '../../../../util/ErrorUtility';
+// import dummyData from './DummyData'
+// import DeclineIcon from '../../../assets/images/decline-icon.png';
+
+// import { DeclineBookingSuccessModal } from './Components/DeclineBookingSuccessModal';
+const screenWidth = Dimensions.get('window').width;
+const modalHeight = (Dimensions.get('window').width / 1.55) * 2;
+
+const SelectedBookingDetails = ({navigation, session, createSession, route}) => {
+    const {delivery} = route.params;
+    navigation.setOptions({
+        headerLeft: () => <HeaderBack />,
+        headerTitle: () => <HeaderTitle label={['Booking Details', '']} fontFamily={CONSTANTS.FONT_FAMILY.REGULAR} />,
+    });
+
+    const dropDownRef = useRef(null)
+
+    const [showBookingModal, setShowBookingModal] = useState(false)
+    const [showSuccessCancelBooking, setShowSuccessCancelBooking] = useState(false)
+    const [showModal, setShowModal] = useState(false)
+    const [showReason, setShowReason] = useState(false)
+    const [showSuccessfull, setShowSuccessfull] = useState(false)
+    const [confirmed, setConfirmed] = useState("");
+
+    const showBookingReason = () => {
+        setShowReason(!showReason)
+        setShowModal(!showModal)
+    }
+    
+    const succefullCancel = () => {
+        setShowReason(!showReason)
+        setShowSuccessfull(!showSuccessfull)
+    }
+
+    // useEffect(() => {
+    //   const oldStatus = session.dummyStatus;
+    //   if(oldStatus == 4) {
+    //     const updateStatus = {
+    //       ...session,
+    //       dummyStatus: oldStatus + 1
+    //     }
+    //     createSession(updateStatus);
+    //   }
+    // }, [])
+
+    const declineBooking = () => {
+      console.log("DECLINED!")
+      setShowBookingModal(false)
+      setShowSuccessCancelBooking(true)
+    }
+
+    const onAccept = (paymentMethodSelected = false) => {
+        // setCaptchaVisible(false);
+        console.log('ON ACCPET DELIVERY');
+        const updateStatus = {
+          ...session,
+          dummyStatus: 2
+        }
+      };
+
+      console.log('zionn', delivery);
+  
+
+    return (
+        <ScrollView style={styles.container} contentContainerStyle={{ flexGrow: 1 }}>
+            <Text>hi</Text>
+        </ScrollView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: CONSTANTS.COLOR.WHITE,
+        marginHorizontal: 0
+    }
+})
+
+const mapStateToProps = (state) => ({
+    session: state.session,
+  });
+  
+const mapDispatchToProps = (dispatch) => ({
+    createSession: (payload) => dispatch({type: 'CREATE_SESSION', payload}),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(SelectedBookingDetails)
+
+
