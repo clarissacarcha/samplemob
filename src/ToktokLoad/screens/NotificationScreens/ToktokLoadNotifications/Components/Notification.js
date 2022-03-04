5251170f4a2fe11b9c59cd035519284b22536cf1
Alvin Raquem <araquem.cloudpanda@gmail.com>
2022-03-04 15:04:53 +0800

    initial load notifications

---
+++ src/ToktokLoad/screens/NotificationScreens/ToktokLoadNotifications/Components/Notification.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Text , View, StyleSheet } from 'react-native';
+import moment from 'moment';
+import CONSTANTS from 'common/res/constants';
+
+const { COLOR , FONT_FAMILY: FONT , FONT_SIZE , SIZE } = CONSTANTS
+
+export const Notification = ({
+    item,
+    index
+})=> {
+    
+    const status = item.type == "postPayLoadSuccessful" ? "Load success" : "Load failed";
+    const dateCreated = moment(item.createdAt).tz('Asia/Manila').format('MMM D, YYYY')
+    const timeCreated = moment(item.createdAt).tz('Asia/Manila').format('hh:mma')
+
+    return (
+        <View style={styles.container}> 
+            <View style={{flex:1,marginRight: 16}}>
+                    <Text style={styles.title}>{status}</Text>
+                    <Text numberOfLines={1} style={styles.message}>{item.body}</Text>
+            </View>
+            <View style={{alignItems:"flex-end",flexBasis: "auto",}}>
+                <Text numberOfLines={1} style={styles.datetime}>{dateCreated}</Text>
+                <Text numberOfLines={1} style={styles.datetime}>{timeCreated}</Text>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 16,
+        flexDirection:"row"
+    },
+    title: {
+        fontFamily: FONT.REGULAR,
+        fontSize: FONT_SIZE.M
+    },
+    message: {
+        fontFamily: FONT.REGULAR,
+        fontSize: FONT_SIZE.S,
+        color: COLOR.DARK
+    },
+    datetime: {
+        fontFamily: FONT.REGULAR,
+        fontSize: FONT_SIZE.S,
+        color: COLOR.DARK
+    }
+})
--- src/ToktokLoad/screens/index.js
+++ src/ToktokLoad/screens/index.js
@@ -1,2 +1,3 @@
 export * from "./GlobalScreens";
+export * from "./NotificationScreens";
 export * from "./RootScreens";
