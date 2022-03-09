30525018c622fc4285c43ae35cee50a8c1d9308d
Alvin Raquem <araquem.cloudpanda@gmail.com>
2022-03-09 18:22:24 +0800

    fix conflict

--- src/ToktokWallet/screens/SendMoneyScreens/ToktokWalletSendMoney/Components/index.js
+++ src/ToktokWallet/screens/SendMoneyScreens/ToktokWalletSendMoney/Components/index.js
@@ -1,4 +1,6 @@
+export * from "./ContextProvider";
 export * from "./EnterAmount";
 export * from "./EnterMobileNo";
 export * from "./EnterNote";
+export * from "./Favorites";
 export * from "./ProceedButton";
