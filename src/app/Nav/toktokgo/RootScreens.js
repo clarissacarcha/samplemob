14145ba85894556e70b12d74e2bc82e8bedbb394
Zion Camba <zcamba@cloudpanda.ph>
2022-03-22 12:41:38 +0800

    done save location/address & retrieve old UI

--- src/app/Nav/toktokgo/RootScreens.js
+++ src/app/Nav/toktokgo/RootScreens.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import SelectedBookingDetails from '../../../ToktokGo/screens/BookingDetails/SelectedBookingDetails';
+import PinLocation from '../../../Toktok/screens/SavedLocationScreens/PinLocation';
 
 export default ({Navigator}) => (
-    <>
-     {/* INSERT SCREENS HERE */}
-     <Navigator.Screen name="SelectedBookingDetails" component={SelectedBookingDetails} />
-    </>
-)
-
+  <>
+    {/* INSERT SCREENS HERE */}
+    <Navigator.Screen name="SelectedBookingDetails" component={SelectedBookingDetails} />
+    <Navigator.Screen name="PinLocation" component={PinLocation} />
+  </>
+);
