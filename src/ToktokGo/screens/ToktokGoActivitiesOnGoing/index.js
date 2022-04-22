720b2079580ab6904d65b17aeec00a9fa6e88c56
Alvin Raquem <araquem.cloudpanda@gmail.com>
2022-04-22 11:52:28 +0800

    enable load in menu

--- src/ToktokGo/screens/ToktokGoActivitiesOnGoing/index.js
+++ src/ToktokGo/screens/ToktokGoActivitiesOnGoing/index.js
@@ -4,7 +4,7 @@ import {useQuery} from '@apollo/react-hooks';
 import {connect} from 'react-redux';
 import {useFocusEffect} from '@react-navigation/native';
 import {COLOR, MEDIUM} from '../../../res/constants';
-import {ActivitiesCard} from '../../components';
+import {ActivitiesCard,SomethingWentWrong} from '../../components';
 import {GET_DELIVERIES, TOKTOK_GO_GRAPHQL_CLIENT} from '../../../graphql';
 import NoData from '../../../assets/images/NoData.png';
 import DummyData from '../../components/DummyData';
