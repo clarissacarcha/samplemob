cc131968f760bfafcea1fa55abf8516c68572cc6
Raven <raveenlucas03@gmail.com>
2022-05-13 16:41:24 +0800

    refactor: commit lint test 3

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
+  "type-enum": [2, "always", ["build", "chore", "ci", "docs", "feat", "fix", "perf", "refactor", "revert", "style", "test"]],
   "scope-enum": [
     2,
     "always",
