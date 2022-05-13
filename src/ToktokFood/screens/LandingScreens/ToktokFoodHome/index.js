debcee043291e476dcf9e8d6a13a1762f5b04496
Raven <raveenlucas03@gmail.com>
2022-05-13 16:36:40 +0800

    feat(refactor): commit lint test 2

--- src/ToktokFood/screens/LandingScreens/ToktokFoodHome/index.js
+++ src/ToktokFood/screens/LandingScreens/ToktokFoodHome/index.js
@@ -8,6 +8,7 @@ import {StyleSheet, View, StatusBar} from 'react-native';
 import HeaderImageBackground from 'toktokfood/components/HeaderImageBackground';
 import HeaderSearchBox from 'toktokfood/components/HeaderSearchBox';
 import HeaderTitle from 'toktokfood/components/HeaderTitle';
+
 // Hooks
 import {useUserLocation} from 'toktokfood/hooks';
 
