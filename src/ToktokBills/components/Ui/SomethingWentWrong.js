0e543e040a4e07abe3312646b3686a48bab839f9
clarkkamorr <camor@cloudpanda.ph>
2022-05-30 14:35:11 +0800

    TKBM-27 The user should be prompted if its internet connection is lost/off while using toktokbills

--- src/ToktokBills/components/Ui/SomethingWentWrong.js
+++ src/ToktokBills/components/Ui/SomethingWentWrong.js
@@ -5,7 +5,7 @@ import {COLOR, DARK, MEDIUM, FONT_SIZE, FONTS} from 'src/res/constants';
 import FIcon from 'react-native-vector-icons/Feather';
 import {moderateScale} from 'toktokbills/helper';
 
-import SomethingWrong from 'src/assets/images/SomethingWentWrong.png';
+import NoInternetConnection from 'src/assets/images/NoInternetConnection.png';
 
 const imageWidth = Dimensions.get('window').width - 200;
 
@@ -13,12 +13,12 @@ export const SomethingWentWrong = ({onRefetch, error}) => {
   const {graphQLErrors, networkError} = error; 
   return (
     <View style={styles.container}>
-      <Image source={SomethingWrong} style={styles.image} resizeMode={'contain'} />
-      <Text style={styles.text}>Oops! Something went wrong...</Text>
-      { networkError && <Text style={styles.text}>Please check your internet connection</Text> }
+      <Image source={NoInternetConnection} style={styles.image} resizeMode={'contain'} />
+      <Text style={styles.textBold}>No Internet Connection</Text>
+      { networkError && <Text style={styles.text}>Please connect to a network to use the app.</Text> }
       {onRefetch && (
         <TouchableOpacity onPress={onRefetch} style={styles.autoFill}>
-          <Text style={{color: "white", fontSize: FONT_SIZE.L, fontFamily: FONTS.BOLD}}>Retry</Text>
+          <Text style={{color: "white", fontSize: FONT_SIZE.L, fontFamily: FONTS.BOLD}}>Try again</Text>
         </TouchableOpacity>
       )}
     </View>
@@ -32,13 +32,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   image: {
-    height: imageWidth,
+    height: imageWidth, 
     width: imageWidth,
     marginBottom: moderateScale(10)
   },
   text: {
-    color: "#787777",
-    fontSize: FONT_SIZE.L,
+    fontSize: FONT_SIZE.M,
+    marginBottom: moderateScale(10)
+  },
+  textBold: {
+    color: "#F6841F",
+    fontSize: FONT_SIZE.XL,
+    fontFamily: FONTS.BOLD,
     marginBottom: moderateScale(10)
   },
   autoFillBox: {
@@ -47,11 +52,11 @@ const styles = StyleSheet.create({
   },
   autoFill: {
     backgroundColor: "#F6841F",
-    borderRadius: 3,
+    borderRadius: 5,
     justifyContent: 'center',
     alignItems: 'center',
-    paddingHorizontal: moderateScale(50),
+    paddingHorizontal: moderateScale(100),
     paddingVertical: moderateScale(10),
-    marginTop: moderateScale(60)
+    marginTop: moderateScale(24)
   },
 });
