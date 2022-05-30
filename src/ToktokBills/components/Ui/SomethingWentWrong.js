3be01057514f97268388021811cb8b02b4001a28
clarkkamorr <camor@cloudpanda.ph>
2022-05-30 15:31:09 +0800

    somethingWentWrong-bills-changes

--- src/ToktokBills/components/Ui/SomethingWentWrong.js
+++ src/ToktokBills/components/Ui/SomethingWentWrong.js
@@ -6,23 +6,39 @@ import FIcon from 'react-native-vector-icons/Feather';
 import {moderateScale} from 'toktokbills/helper';
 
 import NoInternetConnection from 'src/assets/images/NoInternetConnection.png';
+import SomethingWrong from 'src/assets/images/SomethingWentWrong.png';
 
 const imageWidth = Dimensions.get('window').width - 200;
 
 export const SomethingWentWrong = ({onRefetch, error}) => {
   const {graphQLErrors, networkError} = error; 
-  return (
-    <View style={styles.container}>
-      <Image source={NoInternetConnection} style={styles.image} resizeMode={'contain'} />
-      <Text style={styles.textBold}>No Internet Connection</Text>
-      { networkError && <Text style={styles.text}>Please connect to a network to use the app.</Text> }
-      {onRefetch && (
-        <TouchableOpacity onPress={onRefetch} style={styles.autoFill}>
-          <Text style={{color: "white", fontSize: FONT_SIZE.L, fontFamily: FONTS.BOLD}}>Try again</Text>
-        </TouchableOpacity>
-      )}
-    </View>
-  );
+  if (networkError){
+    return (
+      <View style={styles.container}>
+        <Image source={NoInternetConnection} style={styles.image} resizeMode={'contain'} />
+        <Text style={styles.textBold}>No Internet Connection</Text>
+        <Text style={styles.text}>Please connect to a network to use the app.</Text>
+        {onRefetch && (
+          <TouchableOpacity onPress={onRefetch} style={styles.autoFill}>
+            <Text style={{color: "white", fontSize: FONT_SIZE.L, fontFamily: FONTS.BOLD}}>Try again</Text>
+          </TouchableOpacity>
+        )}
+      </View>
+    );
+  } else {
+    return (
+      <View>
+        <Image source={SomethingWrong} style={styles.image} resizeMode={'contain'} />
+        <Text style={styles.text2}>Oops! Something went wrong...</Text>
+        <Text style={styles.text2}>{graphQLErrors[0].message}</Text>
+        {onRefetch && (
+          <TouchableOpacity onPress={onRefetch} style={styles.autoFill2}>
+            <Text style={{color: "white", fontSize: FONT_SIZE.L, fontFamily: FONTS.BOLD}}>Retry</Text>
+          </TouchableOpacity>
+        )}
+      </View>
+    )
+  }
 };
 
 const styles = StyleSheet.create({
@@ -38,6 +54,11 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: FONT_SIZE.M,
+    marginBottom: moderateScale(10)
+  },
+  text2: {
+    color: "#787777",
+    fontSize: FONT_SIZE.L,
     marginBottom: moderateScale(10)
   },
   textBold: {
@@ -57,6 +78,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     paddingHorizontal: moderateScale(100),
     paddingVertical: moderateScale(10),
-    marginTop: moderateScale(24)
+    marginTop: moderateScale(20)
+  },
+  autoFill2: {
+    backgroundColor: "#F6841F",
+    borderRadius: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: moderateScale(50),
+    paddingVertical: moderateScale(10),
+    marginTop: moderateScale(60)
   },
 });
