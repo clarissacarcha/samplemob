983528d0ca98566d9868a44f9ecab3b44b80dfc0
Clarissa Carcha <ccarcha@cloudpanda.ph>
2022-05-31 16:39:04 +0800

    chore: remove unnecessary screens

--- src/app/Nav/toktokbills/GlobalScreens/index.js
+++ src/app/Nav/toktokbills/GlobalScreens/index.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import {
-  ToktokBillsActivityDetails,
-  ToktokBillsEnterPinCode,
-  ToktokBillsReceipt,
-  ToktokBillsTermsAndConditions,
-  ToktokBillsTransactionLogs
-} from 'toktokbills/screens';
-import { Platform } from 'react-native';
-import { moderateScale } from "toktokbills/helper";
+import {ToktokBillsActivityDetails, ToktokBillsReceipt, ToktokBillsTermsAndConditions} from 'toktokbills/screens';
+import {Platform} from 'react-native';
+import {moderateScale} from 'toktokbills/helper';
 
 export default ({Navigator}) => {
   return (
-    <>   
-      <Navigator.Screen 
-        name="ToktokBillsActivityDetails" 
+    <>
+      <Navigator.Screen
+        name="ToktokBillsActivityDetails"
         component={ToktokBillsActivityDetails}
         options={{
-          headerTitleAlign: "center",
+          headerTitleAlign: 'center',
           headerStyle: {
-            shadowColor: "#000",
+            shadowColor: '#000',
             shadowOffset: {
               width: 0,
               height: 2,
@@ -30,31 +24,13 @@ export default ({Navigator}) => {
           },
         }}
       />
-      <Navigator.Screen 
-        name="ToktokBillsEnterPinCode" 
-        component={ToktokBillsEnterPinCode}
-        options={{
-          headerTitleAlign: "center",
-          headerStyle: {
-            shadowColor: "#000",
-            shadowOffset: {
-              width: 0,
-              height: 2,
-            },
-            shadowOpacity: 0.1,
-            shadowRadius: 3,
-            elevation: 5,
-            // height: Platform.OS == "ios" ? moderateScale(60) : moderateScale(80)
-          },
-        }}
-      />
-      <Navigator.Screen 
-        name="ToktokBillsReceipt" 
+      <Navigator.Screen
+        name="ToktokBillsReceipt"
         component={ToktokBillsReceipt}
         options={{
-          headerTitleAlign: "center",
+          headerTitleAlign: 'center',
           headerStyle: {
-            shadowColor: "#000",
+            shadowColor: '#000',
             shadowOffset: {
               width: 0,
               height: 2,
@@ -66,31 +42,13 @@ export default ({Navigator}) => {
           },
         }}
       />
-      <Navigator.Screen 
-        name="ToktokBillsTermsAndConditions" 
+      <Navigator.Screen
+        name="ToktokBillsTermsAndConditions"
         component={ToktokBillsTermsAndConditions}
         options={{
-          headerTitleAlign: "center",
+          headerTitleAlign: 'center',
           headerStyle: {
-            shadowColor: "#000",
-            shadowOffset: {
-              width: 0,
-              height: 2,
-            },
-            shadowOpacity: 0.1,
-            shadowRadius: 3,
-            elevation: 5,
-            // height: Platform.OS == "ios" ? moderateScale(60) : moderateScale(80)
-          },
-        }}
-      />
-      <Navigator.Screen 
-        name="ToktokBillsTransactionLogs" 
-        component={ToktokBillsTransactionLogs}
-        options={{
-          headerTitleAlign: "center",
-          headerStyle: {
-            shadowColor: "#000",
+            shadowColor: '#000',
             shadowOffset: {
               width: 0,
               height: 2,
