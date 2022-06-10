2bb0aa4716271c6fd75ec380f93ef5dc865acda9
noephiliprestum <noephilip.restum@bicol-u.edu.ph>
2022-06-10 14:26:52 +0800

    fidelity(mdfi_37)_v2: add padding

--- src/ToktokBills/screens/RootScreens/ToktokBillsPaymentSummary/Components/PaymentDetails/index.js
+++ src/ToktokBills/screens/RootScreens/ToktokBillsPaymentSummary/Components/PaymentDetails/index.js
@@ -174,6 +174,7 @@ const styles = StyleSheet.create({
     fontSize: FONT_SIZE.M,
     fontFamily: FONT.REGULAR,
     width: '50%',
+    paddingRight: moderateScale(10),
   },
   description: {
     textAlign: 'right',
