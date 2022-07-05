1de815710587001c66fb47a4e8a8faaa5fb51749
Anthony Geronimo <ageronimo@cloudpanda.ph>
2022-07-05 18:11:18 +0800

    added refcode already exists and update of session after ref code input

--- src/app/Nav/superApp/RootScreens.js
+++ src/app/Nav/superApp/RootScreens.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 import SuperAppPromosScreen from '../../../SuperApp/screens/SuperAppPromos/SuperAppPromos';
-import {ReferralScreen, SelectedVoucherScreen, VoucherScreen} from '../../../SuperApp/screens';
+import {SelectedVoucherScreen, VoucherScreen} from '../../../SuperApp/screens';
+import ReferralScreen from '../../../SuperApp/screens/ReferralScreen/ReferralScreen';
+import ReferralExistScreen from '../../../SuperApp/screens/ReferralScreen/ReferralExistScreen';
 
 export default ({Navigator}) => (
   <>
@@ -9,5 +11,6 @@ export default ({Navigator}) => (
     <Navigator.Screen name="ReferralScreen" component={ReferralScreen} options={{headerShown: false}} />
     <Navigator.Screen name="VoucherScreen" component={VoucherScreen} options={{headerShown: false}} />
     <Navigator.Screen name="SelectedVoucherScreen" component={SelectedVoucherScreen} options={{headerShown: false}} />
+    <Navigator.Screen name="ReferralExistScreen" component={ReferralExistScreen} options={{headerShown: false}} />
   </>
 );
