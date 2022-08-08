eb93ed1af2a85eb89b4357462247f6e07cdc1aef
Ding Harley Catubag <dcatubag@cloudpanda.ph>
2022-08-08 10:17:51 +0800

    fix: misspelled export

--- src/ToktokWallet/screens/CashOutOTCScreens/index.js
+++ src/ToktokWallet/screens/CashOutOTCScreens/index.js
@@ -1,4 +1,4 @@
 export * from './ToktokWalletCashOutOTCHome';
 export * from './ToktokWalletCashOutOTCPaymentSummary';
-export * from './ToktokWalletCashOutOTCReceipt';
+export * from './ToktokWalletCashoutOTCReceipt';
 export * from './ToktokWalletCashOutOTCTransaction';
