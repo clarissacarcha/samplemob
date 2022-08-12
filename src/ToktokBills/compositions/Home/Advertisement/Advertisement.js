dc59540c95eea03d90a6861e56886f1f21479e6e
clarkkamorr <camor@cloudpanda.ph>
2022-08-12 16:05:38 +0800

    chore(fix): remove unnecessary codes

--- src/ToktokBills/compositions/Home/Advertisement/Advertisement.js
+++ src/ToktokBills/compositions/Home/Advertisement/Advertisement.js
@@ -46,13 +46,14 @@ export const Advertisement = (props: PropsType): React$Node => {
           layout="default"
           data={ads}
           renderItem={({item, index}) => <DisplayImage item={item} index={index} />}
-          autoplay={ads.length > 1 && true}
-          loop={ads.length > 1 && true}
+          autoplay={ads.length > 1}
+          loop={ads.length > 1}
           autoplayDelay={0}
           autoplayInterval={5000}
         />
       ) : (
         <List
+          scrollEnabled={ads.length > 1}
           horizontal
           showsHorizontalScrollIndicator={false}
           data={ads}
