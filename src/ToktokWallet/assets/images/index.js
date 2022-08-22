dbd1880c3e9e722350414a39d4310d1b10c0dd3b
Clarissa Carcha <ccarcha@cloudpanda.ph>
2022-08-22 17:04:01 +0800

    feat(TKWM-205): display additional instructions in the Step 3 of KYC Registration process

--- src/ToktokWallet/assets/images/index.js
+++ src/ToktokWallet/assets/images/index.js
@@ -30,3 +30,14 @@ export const empty_images = {
   empty_search: require('./empty-screens/empty-search.png'),
   empty_fave: require('./empty-screens/empty-fave.png'),
 };
+
+export const kyc_intructions = {
+  id_good: require('./kyc-instructions/id-good.png'),
+  id_not_blurry: require('./kyc-instructions/id-not-blurry.png'),
+  id_not_cut: require('./kyc-instructions/id-not-cut.png'),
+  id_not_reflective: require('./kyc-instructions/id-not-reflective.png'),
+  selfie_correct: require('./kyc-instructions/selfie-correct.png'),
+  selfie_with_id_correct: require('./kyc-instructions/selfie-with-id-correct.png'),
+  selfie_with_id_wrong: require('./kyc-instructions/selfie-with-id-wrong.png'),
+  selfie_wrong: require('./kyc-instructions/selfie-wrong.png'),
+};
