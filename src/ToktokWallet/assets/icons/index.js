a51721a19fa0255d8b0bba3cfcf58f41cb5b5198
Clarissa Carcha <ccarcha@cloudpanda.ph>
2022-08-22 16:24:28 +0800

    feat(TKWM-244): revamp toktokwallet contact us

--- src/ToktokWallet/assets/icons/index.js
+++ src/ToktokWallet/assets/icons/index.js
@@ -6,3 +6,7 @@ export const circle_check = require('./circle-check.png');
 export const bank_icon = require('./bank-icon.png');
 export const schedule_icon = require('./schedule-icon.png');
 export const upload_icon = require('./upload-icon.png');
+export const contact_us = {
+  email: require('./contact-us/email.png'),
+  phone: require('./contact-us/phone.png'),
+};
