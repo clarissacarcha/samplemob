dbe68ae99c218ec8b6ee4b49e537cf0b4f8d66db
Clarissa Carcha <ccarcha@cloudpanda.ph>
2022-08-24 15:43:20 +0800

    feat(TKWM-240): revamp payment chart

--- src/ToktokWallet/screens/SettingScreens/ToktokWalletPaymentChart/Components/CustomTable/index.js
+++ src/ToktokWallet/screens/SettingScreens/ToktokWalletPaymentChart/Components/CustomTable/index.js
@@ -1,150 +1,174 @@
-import React from 'react'
-import {View,Text,StyleSheet,Platform,Dimensions,StatusBar,ScrollView} from 'react-native'
-import CONSTANTS from 'common/res/constants'
-import {moderateScale,scale} from 'toktokwallet/helper'
+import React from 'react';
+import {View, Text, StyleSheet, ScrollView} from 'react-native';
+import CONSTANTS from 'common/res/constants';
+import {moderateScale, numFormatter} from 'toktokwallet/helper';
 
-const { COLOR , FONT_FAMILY: FONT , FONT_SIZE } = CONSTANTS
-const { width , height } = Dimensions.get("window")
+const {COLOR, FONT_FAMILY: FONT, FONT_SIZE} = CONSTANTS;
 
-const Header = ({data})=> {
+const Header = ({headerData}) => {
+  const getCellWidth = index => {
+    switch (index) {
+      case 0:
+        return '25%';
+      case 1:
+        return '30%';
+      case 2:
+        return '13%';
+      case 3:
+        return '15%';
+      case 4:
+        return '17%';
+    }
+  };
 
-    return (
-       <View style={styles.headings}>
-           {
-               data.map((header,index)=>{
-                   let width = "25%";
-                   switch(index){
-                       case 0:
-                            width = "25%"
-                            break
-                       case 1:
-                            width = "28%"
-                            break
-                       case 2:
-                            width = "32%"
-                            break
-                       default:
-                            width = "18%"
-                            break
-                   }
-                   return (
-                    <View style={[styles.title , {width}]}>
-                            <Text style={styles.headerTitle}>{header}</Text>
-                    </View>
-                   )
-               })
-           }
-         
-       </View>
-    )
-}
+  return (
+    <View style={styles.headerContainer}>
+      {headerData.map((header, index) => {
+        let cellwidth = getCellWidth(index);
+        return (
+          <View style={{width: cellwidth}}>
+            <Text style={styles.headerText}>{header}</Text>
+          </View>
+        );
+      })}
+    </View>
+  );
+};
 
-const Column = ({data, rowindex})=> {
+const PaymentChart = ({paymentChart}) => {
+  return (
+    <View style={styles.paymentChartContainer}>
+      {paymentChart.map((details, index) => {
+        return (
+          <View
+            style={{...styles.paymentChartSubContainer, ...{backgroundColor: index % 2 === 1 ? 'white' : '#FFF4EB'}}}>
+            <View style={styles.cell25}>
+              <Text style={styles.paymentChartText}>{details.partnerName}</Text>
+            </View>
+            <View style={styles.cell100}>
+              {details.paymentChartRanges.map((range, ind) => {
+                const {from, to, fee, toktokServiceFee, rate} = range;
+                const computedFee = `${parseFloat(fee) + parseFloat(toktokServiceFee)} + ${rate}%`;
 
-    return (
-        <>
-        {
-            data.map((content,rowIndex)=> {
-
-                let rowBody = []
-                for (const [key, value] of Object.entries(content)) {
-                    rowBody.push(value)    
-                }
-    
                 return (
-                    <View style={styles.rowCells}>
-                       {
-                           rowBody.map((row,index)=> {
-                               let cellwidth
-                               let cellAlign = "left"
-                               if(index == 0){
-                                 cellwidth = "33%"
-                               }
-                               if(index == 1){
-                                  cellwidth = "33%"
-                               }
-                               if(index == 2){
-                                 cellwidth = "33%"
-                               }
-                               if(index == 3){
-                                  cellwidth = "24%"
-                               }
-                               return (
-                                   <View style={{
-                                                width:cellwidth,
-                                                justifyContent:"center",
-                                                backgroundColor: rowIndex == 0 ? COLOR.YELLOW : rowIndex%2 == 0 ? "#FEF7E6" : "white",
-                                                paddingVertical: 10
-                                            }}>
-                                        {
-                                            row.map((displayText)=>(
-                                                <Text style={[
-                                                    styles.headerTitle , 
-                                                    {
-                                                        fontFamily: rowIndex == 0 ? FONT.BOLD : FONT.REGULAR,
-                                                        fontSize: rowIndex == 0 ? moderateScale(FONT_SIZE.M) : moderateScale(FONT_SIZE.S),
-                                                        textAlign: "center" 
-                                                    }
-                                                ]}>
-                                                    {displayText
-                                                }</Text>
-                                            ))
-                                        }
-                                   </View>
-                                  
-                               )
-                           })
-                       }
-                           
+                  <View
+                    style={{
+                      ...styles.rangeContainer,
+                      ...{marginBottom: ind === details.paymentChartRanges.length - 1 ? 0 : 5},
+                    }}>
+                    <View style={styles.cell30}>
+                      <Text style={styles.paymentChartText}>
+                        ₱{numFormatter(from)} - ₱{numFormatter(to)}
+                      </Text>
                     </View>
-                )
-            })
-        }
-        </>
-    )
-}
+                    <View style={styles.cell13}>
+                      <Text style={styles.paymentChartText}>
+                        {details.partnerType.name === 'Over the Counter' ? `₱${computedFee}` : '--'}
+                      </Text>
+                    </View>
+                    <View style={styles.cell15}>
+                      <Text style={styles.paymentChartText}>
+                        {details.partnerType.name === 'Online Bank' ? `₱${computedFee}` : '--'}
+                      </Text>
+                    </View>
+                    <View style={styles.cell17}>
+                      <Text style={{...styles.paymentChartText, ...styles.textCenter}}>
+                        {details.partnerType.name === 'Payment thru Mobile/E-Wallet' ? `₱${computedFee}` : '--'}
+                      </Text>
+                    </View>
+                  </View>
+                );
+              })}
+            </View>
+          </View>
+        );
+      })}
+    </View>
+  );
+};
 
-
-export const CustomTable = ({headerData = [], rowsData = []})=> {
-
-    return (
-        <ScrollView
-            style={styles.container}
-        >
-            <Column
-                data={rowsData}
-            />
-        </ScrollView>
-    )
-}
+export const CustomTable = ({headerData = [], rowsData = [], data = []}) => {
+  return (
+    <ScrollView style={styles.container}>
+      {data.map(item => {
+        const {paymentChart} = item;
+        if (paymentChart.length === 0) return null;
+        return (
+          <View style={styles.contentContainer}>
+            <Text style={styles.service}>{item.description}</Text>
+            <Header headerData={headerData} />
+            <PaymentChart paymentChart={paymentChart} />
+          </View>
+        );
+      })}
+    </ScrollView>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor:"white",
-        marginTop: 16,
-    },
-    headings: {
-        flexDirection:"row",
-        // borderWidth: 1,
-    },
-    title: {
-        width: width / 4,
-        justifyContent:'center',
-        alignItems:"center",
-        flex: 1,
-        paddingVertical: 5,
-        backgroundColor:COLOR.YELLOW,
-    },
-    headerTitle: {
-        fontFamily: FONT.BOLD,
-        fontSize: scale(FONT_SIZE.S),
-        paddingHorizontal: 5,
-        textAlign:"center"
-    },
-    rowCells: {
-        flex: 1,
-        flexDirection:"row",
-        paddingVertical: 2,
-    }
-})
+  container: {
+    marginTop: moderateScale(20),
+  },
+  contentContainer: {
+    marginBottom: moderateScale(20),
+  },
+  service: {
+    fontFamily: FONT.BOLD,
+    marginBottom: moderateScale(10),
+  },
+  headerContainer: {
+    flexDirection: 'row',
+    flexShrink: 1,
+    backgroundColor: COLOR.ORANGE,
+    borderTopLeftRadius: moderateScale(5),
+    borderTopRightRadius: moderateScale(5),
+    alignItems: 'center',
+    padding: moderateScale(10),
+  },
+  headerText: {
+    fontSize: FONT_SIZE.S,
+    fontFamily: FONT.SEMI_BOLD,
+    marginHorizontal: moderateScale(5),
+    color: COLOR.WHITE,
+  },
+  paymentChartContainer: {
+    marginVertical: moderateScale(10),
+  },
+  paymentChartSubContainer: {
+    flexDirection: 'row',
+    paddingHorizontal: moderateScale(10),
+    paddingVertical: moderateScale(20),
+    alignItems: 'center',
+    borderRadius: moderateScale(5),
+  },
+  paymentChartText: {
+    fontSize: FONT_SIZE.S,
+    marginHorizontal: moderateScale(5),
+  },
+  rangeContainer: {
+    flexDirection: 'row',
+    flex: 1,
+    alignItems: 'center',
+  },
+  textCenter: {
+    textAlign: 'center',
+  },
+  cell25: {
+    width: '25%',
+  },
+  cell100: {
+    width: '100%',
+  },
+  cell13: {
+    width: '13%',
+  },
+  cell17: {
+    width: '17%',
+  },
+  cell30: {
+    width: '30%',
+  },
+  cell15: {
+    width: '15%',
+  },
+});
--- src/ToktokWallet/screens/SettingScreens/ToktokWalletPaymentChart/index.js
+++ src/ToktokWallet/screens/SettingScreens/ToktokWalletPaymentChart/index.js
@@ -1,172 +1,88 @@
-import React from 'react'
-import {View,Text,StyleSheet,Platform,Dimensions,StatusBar,ScrollView} from 'react-native'
-import {HeaderBack, HeaderTitle,YellowButton } from 'src/revamp';
-import {Separator,CheckIdleState} from 'toktokwallet/components'
-import CONSTANTS from 'common/res/constants'
+import React, {useEffect, useState} from 'react';
+import {View, Text, StyleSheet, Platform, Dimensions, RefreshControl, ScrollView} from 'react-native';
+//GRAPHQL
+import {useLazyQuery} from '@apollo/react-hooks';
+import {TOKTOK_WALLET_GRAPHQL_CLIENT} from 'src/graphql';
+import {GET_PAYMENT_CHART} from 'toktokwallet/graphql';
+//COMPONENTS
+import {
+  HeaderBack,
+  HeaderTitleRevamp,
+  CheckIdleState,
+  LoadingIndicator,
+  SomethingWentWrong,
+} from 'toktokwallet/components';
+import CONSTANTS from 'common/res/constants';
 
 //SELF IMPORTS
-import {
-    CustomTable,
-} from "./Components";
+import {CustomTable} from './Components';
 
-const { COLOR , FONT_FAMILY: FONT , FONT_SIZE } = CONSTANTS
-const { width , height } = Dimensions.get("window")
+const {COLOR, FONT_FAMILY: FONT, FONT_SIZE} = CONSTANTS;
+const {width, height} = Dimensions.get('window');
 
-export const ToktokWalletPaymentChart = ({navigation})=> {
+export const ToktokWalletPaymentChart = ({navigation}) => {
+  navigation.setOptions({
+    headerLeft: () => <HeaderBack />,
+    headerTitle: () => <HeaderTitleRevamp label={'Payment Chart'} />,
+  });
 
-    navigation.setOptions({
-        headerLeft: () => <HeaderBack color={COLOR.YELLOW}/>,
-        headerTitle: () => <HeaderTitle label={['Payment Chart', '']} />,
-    });
+  const [paymentChart, setPaymentChart] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
 
+  const [getPaymentChartList, {date, error, loading}] = useLazyQuery(GET_PAYMENT_CHART, {
+    fetchPolicy: 'network-only',
+    client: TOKTOK_WALLET_GRAPHQL_CLIENT,
+    onError: () => {
+      setRefreshing(false);
+    },
+    onCompleted: data => {
+      setPaymentChart(data.getPaymentChartList);
+      setRefreshing(false);
+    },
+  });
+
+  useEffect(() => {
+    getPaymentChartList();
+  }, [getPaymentChartList]);
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    getPaymentChartList();
+  };
+
+  if (loading) {
     return (
-        <CheckIdleState>
-            <Separator/>
-            <ScrollView
-                style={styles.container}
-                showsVerticalScrollIndicator={false}
-            >
-                <CustomTable
-                    headerData={["Partner","Method","Transaction Range","Transaction Fee"]}
-                    rowsData={[
-                        {
-                            firstCol: ["Partner"],
-                            secondCol: ["Method"],
-                            thirdCol: ["Transaction","Fee"],
-                        },
-                        {
-                            firstCol: ["DRAGONPAY"],
-                            secondCol: ["Online Bank"],
-                            thirdCol: ["25"],
-                        },
-                        {
-                            firstCol: ["DRAGONPAY"],
-                            secondCol: ["Over the", "counter Bank"],
-                            thirdCol: ["30"],
-                        },
-                        {
-                            firstCol: ["DRAGONPAY"],
-                            secondCol: ["Over the", "counter Non-Bank"],
-                            thirdCol: ["35"],
-                        },
-                    ]}
-                    rowsDataOld={
-                        [
-                            {
-                                firstCol: ["Partner"],
-                                secondCol: ["Method"],
-                                thirdCol: ["Transaction","Range"],
-                                fourthCol: ["Transaction","Fee"],
-                            },
-                            {
-                                firstCol: ["UNIONBANK"],
-                                secondCol: ["Over the Counter","Online Bank"],
-                                thirdCol: ["--","--"],
-                                fourthCol: ["30","25"],
-                            },
-                            {
-                                firstCol:  ["BDO"],
-                                secondCol: ["Over the Counter","Online Bank"],
-                                thirdCol: ["--","--"],
-                                fourthCol: ["30","25"],
-                            },
-                            {
-                                firstCol: ["CHINABANK"],
-                                secondCol: ["Online Bank","ATM"],
-                                thirdCol: ["--","--"],
-                                fourthCol: ["25","25"],
-                            },
-                            {
-                                firstCol: ["RCBC"],
-                                secondCol: ["Over the Counter","Online Bank"],
-                                thirdCol: ["--","--"],
-                                fourthCol: ["30","25"],
-                            },
-                            {
-                                firstCol: ["PNB"],
-                                secondCol: ["Over the Counter","Online Bank"],
-                                thirdCol: ["--","--"],
-                                fourthCol: ["30","25"],
-                            },
-                            {
-                                firstCol: ["UCPB"],
-                                secondCol: ["Over the Counter","Online Bank"],
-                                thirdCol: ["--","--"],
-                                fourthCol: ["30","25"],
-                            },
-                            {
-                                firstCol: ["METROBANK"],
-                                secondCol: ["Online Bank"],
-                                thirdCol: ["--"],
-                                fourthCol: ["25"],
-                            },
-                            {
-                                firstCol: ["PBCOM"],
-                                secondCol: ["Online Bank"],
-                                thirdCol: ["--"],
-                                fourthCol: ["25"],
-                            },
-                            {
-                                firstCol: ["ROBINSONS"],
-                                secondCol:["Over the Counter"],
-                                thirdCol: ["--"],
-                                fourthCol: ["30"],
-                            },
-                            {
-                                firstCol:  ["M LHUILLER","","","","","",""],
-                                secondCol: ["Over the Counter","","","","","",""],
-                                thirdCol: ["PHP .01 - 2,500","PHP 2,500 - 5,000","PHP 5,000.01 - 10K","PHP 10,000.01 - 20k","PHP 20,000.01 - 30K","PHP 30,000.01 - 40k","PHP 40,000.01 - 50k"],
-                                fourthCol: ["35","35","35","35","40","50","60"],
-                            },
-                            {
-                                firstCol: ["LBC"],
-                                secondCol: ["Over the Counter"],
-                                thirdCol: ["PHP .01 - 25k"],
-                                fourthCol: ["35"],
-                            },
-                            {
-                                firstCol: ["ECPAY"],
-                                secondCol: ["Over the Counter"],
-                                thirdCol: ["--"],
-                                fourthCol: ["35"],
-                            },
-                            {
-                                firstCol: ["ALIPAY"],
-                                secondCol: ["E-Wallets / Mobile Payments"],
-                                thirdCol: ["--"],
-                                fourthCol: ["2% + 25"],
-                            },
-                            {
-                                firstCol: ["WECHAT"],
-                                secondCol: ["E-Wallets / Mobile Payments"],
-                                thirdCol: ["--"],
-                                fourthCol: ["2% + 25"],
-                            },
-                            {
-                                firstCol: ["GCASH"],
-                                secondCol: ["E-Wallets / Mobile Payments"],
-                                thirdCol: ["--"],
-                                fourthCol: ["2% + 25"],
-                            },
-                            {
-                                firstCol:  ["IPAY88"],
-                                secondCol:["Credit/Debit Card"],
-                                thirdCol: ["--"],
-                                fourthCol: ["4.4% + 35"],
-                            },
-                        ]
-                    }
-                />
-                <View style={{height: 50}}/>
-            </ScrollView>
-        </CheckIdleState>
-    )
-}
+      <View style={styles.container}>
+        <LoadingIndicator isLoading={true} isFlex />
+      </View>
+    );
+  }
+  if (error) {
+    return (
+      <View style={styles.container}>
+        <SomethingWentWrong error={error} />
+      </View>
+    );
+  }
+  return (
+    // <CheckIdleState>
+    <ScrollView
+      style={styles.container}
+      showsVerticalScrollIndicator={false}
+      refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
+      <CustomTable
+        data={paymentChart}
+        headerData={['Partner', 'Payment Range', 'OTC Fee', 'Online Bank Fee', 'Payment through mobile']}
+      />
+    </ScrollView>
+    // </CheckIdleState>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor:"white",
-        paddingHorizontal: 5,
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    paddingHorizontal: 16,
+  },
+});
