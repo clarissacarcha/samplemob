0c5eeb8bd54144f036f8cb3a1546bf67e6eca0a0
Clarissa Carcha <ccarcha@cloudpanda.ph>
2022-08-25 12:38:25 +0800

    feat(settings): revamp security and privacy

--- src/ToktokWallet/screens/SettingScreens/HelpCentreScreens/ToktokWalletHelpCentreSecurityPrivacy/index.js
+++ src/ToktokWallet/screens/SettingScreens/HelpCentreScreens/ToktokWalletHelpCentreSecurityPrivacy/index.js
@@ -1,49 +1,31 @@
-import React from 'react'
-import {View,Text,StyleSheet,Platform,Dimensions,StatusBar,Image, TouchableOpacity} from 'react-native'
-import {Separator,CheckIdleState, HeaderBack, HeaderTitleRevamp ,} from 'toktokwallet/components'
-import CONSTANTS from 'common/res/constants'
-import { LayeredSecurityMoneyProtected } from "./Components";
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+import {CheckIdleState, HeaderBack, HeaderTitleRevamp} from 'toktokwallet/components';
+import CONSTANTS from 'common/res/constants';
+import {LayeredSecurityMoneyProtected} from './Components';
 
-const { COLOR, FONT_FAMILY: FONT , FONT_SIZE } = CONSTANTS
+const {COLOR} = CONSTANTS;
 
-const Card = (props) => {
-    return (
-        <>
-            <Separator />    
-                <TouchableOpacity onPress={props.onPress || null} style={{flexDirection: 'row', paddingVertical: 8, paddingHorizontal: 18}}>
-                    <View style={{flex: 3, justifyContent: 'center',paddingVertical: 20,}}>
-                        <Text style={{fontFamily: FONT.REGULAR, fontSize: FONT_SIZE.L}}>{props.title}</Text>
-                        {/* <Text style={{fontFamily: FONT.REGULAR, fontSize: FONT_SIZE.S}}>{props.content}</Text> */}
-                    </View>
-                    {/* <View style={{flex: 1.5, flexDirection: 'row-reverse'}}>
-                        <Image style={{margin: 8, resizeMode: 'contain', width: 80, height: 90}} source={props.imageSource}/>
-                    </View> */}
-                </TouchableOpacity>
-        </>
-    )
-} 
+export const ToktokWalletHelpCentreSecurityPrivacy = ({navigation}) => {
+  navigation.setOptions({
+    headerLeft: () => <HeaderBack />,
+    headerTitle: () => <HeaderTitleRevamp label={'Privacy Policy'} />,
+  });
 
-export const ToktokWalletHelpCentreSecurityPrivacy = ({navigation})=> {
-
-    navigation.setOptions({
-        headerLeft: () => <HeaderBack/>,
-        headerTitle: () => <HeaderTitleRevamp label={['Privacy and Policy', '']} />,
-    });
-
-    return (
-        <CheckIdleState>
-        <View style={styles.container}>
-            <View style={{flex: 1}}>    
-                <LayeredSecurityMoneyProtected/>
-            </View>
+  return (
+    <CheckIdleState>
+      <View style={styles.container}>
+        <View style={{flex: 1}}>
+          <LayeredSecurityMoneyProtected />
         </View>
-        </CheckIdleState>
-    )
-}
+      </View>
+    </CheckIdleState>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: COLOR.WHITE
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: COLOR.WHITE,
+  },
+});
