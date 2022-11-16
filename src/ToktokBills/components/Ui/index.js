c12807c7ec63597a56095d0d027b4684fcadc9ab
Clarissa Carcha <ccarcha@cloudpanda.ph>
2022-11-16 12:02:35 +0800

    fix(billers): no image

--- src/ToktokBills/components/Ui/index.js
+++ src/ToktokBills/components/Ui/index.js
@@ -1,5 +1,6 @@
 export * from './ActivityCard';
 export * from './EmptyList';
+export * from './PolicyNote';
 export * from './Separator';
 export * from './SomethingWentWrong';
 export * from './ReceiptSeparator';
